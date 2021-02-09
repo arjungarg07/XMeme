38f27d32e625bcd34a74406c3913b6aacde2f2ce
arjungarg07 <arjun.garg.cse01@gmail.com>
2021-02-09 23:22:01 +0530

    added config

---
+++ src/config/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+    port: process.env.PORT,
+    db: {
+        production: {
+
+        },
+        development: {
+            host: process.env.DEV_DB_HOST,
+            port: process.env.DEV_DB_PORT,
+            username: process.env.DEV_DB_USERNAME,
+            password: process.env.DEV_DB_PASSWORD,
+            pool: {
+                max: 20,
+                min: 5,
+            },
+        },
+    },
+}
