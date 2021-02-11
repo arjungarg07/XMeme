d327b147a1619dd47e7a3b45d78c34fa4a27332b
arjungarg07 <arjun.garg.cse01@gmail.com>
2021-02-11 19:12:14 +0530

    updated client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Meme from './components/Meme';
+import MemesContent from './components/MemesContent';
 
 function App() {
   return (
     <div className = 'container'>
-    <Meme/>
+    <MemesContent/>
     </div>
   );
 }
