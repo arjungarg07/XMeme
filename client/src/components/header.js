ad157ffd7cd745827d4cb6f41002b230af13a059
arjungarg07 <arjun.garg.cse01@gmail.com>
2021-02-14 01:58:21 +0530

    updated client

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -3,7 +3,7 @@ import Badge from "react-bootstrap/Badge";
 
 const Header = () => (
   <h1 className="bg-gray-100 mx-2 w-full position-sticky px-40 py-2 top-0 z-10">
-    Meme Stream <Badge variant="secondary">Crio</Badge>
+    Meme Stream <Badge variant="secondary">CWoD</Badge>
   </h1>
 );
 
