48fc3c2ea73ffff736c2f2af6ca0367f85aaff33
arjungarg07 <arjun.garg.cse01@gmail.com>
2021-02-14 22:14:18 +0530

    updated server

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,7 +1,16 @@
 module.exports = {
   port: process.env.PORT,
   db: {
-    production: {},
+    production: {
+      host: process.env.DATABASE_URL,
+      port: process.env.PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PWD,
+      pool: {
+        max: 20,
+        min: 5,
+      },
+    },
     development: {
       host: process.env.DEV_DB_HOST,
       port: process.env.DEV_DB_PORT,
