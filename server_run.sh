2a9e43bb1f5fe4f25a5e979d7406c28b79da2b92
Apoorv Garg <apoorvgarg@Apoorvs-MacBook-Pro.local>
2021-02-17 20:45:17 +0530

    added if condition

--- server_run.sh
+++ server_run.sh
@@ -3,9 +3,10 @@
 
 cd server/src/
 
-
-sed '11,17d' api/routes/memes.js >> api/routes/memes1.js
-mv  api/routes/memes1.js api/routes/memes.js
+if grep -q RegExp "api/routes/memes.js"; then
+    sed '11,17d' api/routes/memes.js >> api/routes/memes1.js
+    mv  api/routes/memes1.js api/routes/memes.js
+fi
 
 # Setup DB or any other environment variables you want to setup.
 
